b865662fcb4f4d5ffb0bcf3c4ec33ecc4a863b34
Charlie Jacomme <jacomme@crans.org>
2018-07-27 15:53:23 +0200

    update

--- src/NC_Gasbi.ml
+++ src/NC_Gasbi.ml
@@ -1,8 +1,25 @@
-open List
-open Num
-open MatSig
+(* Grobner basis computations for K[X]-module *)
+#use "topfind";;
+#require "num";;
+ (* Imports and abbreviations *)
 
+    
+open List;;
+open Num;;
 
+(*
+open NormField;;
+open Util;;
+open ExprUtils;;
+open Expr;;
+open Type;;
+
+let log_i _ls  = ()
+*)
+(* ------------------------------------------------------------------------- *)
+(*  Utility functions                                                        *)
+(* ------------------------------------------------------------------------- *)
+  
 let rec itlist f l b =
   match l with
     [] -> b
@@ -29,81 +46,30 @@ let rec distinctpairs l =
 (* ------------------------------------------------------------------------- *)
 (* Defining polynomial types                                                 *)
 (* ------------------------------------------------------------------------- *)
-    
-type id_var = int
-type vars = id_var list
 
-module type MON = sig
-    type shape
-    val default_shape : shape
-    val mult_shape_compat : shape -> shape -> bool
-    val neg_shape : shape
-    val mult_shape : shape -> shape -> shape
-    val shape_eq : shape -> shape -> bool
-    val shape_of_expr : Expr.expr -> shape
-    val id_of_shape : shape -> Expr.expr
-    type mon = {
-        coeff : Num.num;
-        vars : vars;
-        length : int;
-        size : shape
-    }
+type id_var = int;;
 
-    type pol = mon list
+type id_size = int;;
 
-    val pp_pol : pol -> string
-    val pp_shape : shape -> string
-end
+type vars = id_var list;;
 
-module MkMon (Data : MATDATA) : MON = struct
-    type shape = Data.shape
-    let default_shape = Data.default_shape
-    let mult_shape_compat = Data.mult_shape_compat
-    let neg_shape = Data.neg_shape
-    let mult_shape = Data.mult_shape
-    let shape_eq = Data.shape_eq
-    let shape_of_expr = Data.shape_of_expr
-    let id_of_shape = Data.id_of_shape
-    type mon = {
-        coeff : Num.num;
-        vars : vars;
-        length : int;
-        size : shape
-    }
+type mon =
+  { coeff : Num.num;
+    vars : vars;
+    length : int;
+    size : id_size * id_size;
+  };;
 
-    let pp_shape = Data.pp_shape
+type pol = mon list;;
 
-    type pol = mon list
-
-    let pp_pol (p : pol) : string =
-        "[" ^
-        (String.concat ";" (List.map (fun mon ->
-            "{coeff=Num.Int 1; vars = [" ^ (String.concat ";" (List.map
-            string_of_int mon.vars)) ^ "]; size="^(Data.pp_shape mon.size)^"; length="^(string_of_int
-            (List.length mon.vars))^"}") p ))
-        ^ "]"
-
-  (*      "Poly length: " ^ (string_of_int (List.length p)) ^ ": " ^
-        (String.concat "\n" (
-            List.map (fun mon ->
-                "Mon of vars: " ^
-                (String.concat " " (List.map string_of_int mon.vars))
-                ^ " coeff = " ^ (Num.string_of_num mon.coeff)
-                ^ " length = " ^ (string_of_int mon.length)
-                ^ " size = " ^ (Data.pp_shape mon.size)) p )) *)
-end
-
-module MkGasbi = functor (Mon : MON) -> struct
-
-open Mon
 (* type pol_i = mon list * Expr.expr; *)
 
 type i_var_set = int list;;
 
-let mk_vmon (i:id_var) (size: Mon.shape) : Mon.mon=
-  {Mon.coeff = Num.Int 1; Mon.vars = [i]; Mon.length = 1; size};;
+let mk_vmon (i:id_var) (size: id_size*id_size) :mon=
+  {coeff = Num.Int 1; vars = [i]; length = 1; size};;
 
-let is_null_mon (m:Mon.mon) =
+let is_null_mon (m:mon) =
    m.length=0 || (List.for_all (fun var -> var<0) m.vars)
 
 let is_null (p:pol) : bool =
@@ -136,7 +102,7 @@ let null_mon =
    { coeff = Int 0;
     vars = [];
     length = 0;
-    size = default_shape;
+    size = (1,1);
   };;
 (* ------------------------------------------------------------------------- *)
 (* Operations on monomials.                                                  *)
@@ -146,19 +112,18 @@ let veq_mon (m1:mon) (m2:mon) =
   (m1.length = m2.length ) && m1.vars=m2.vars;;
 
 let mmul (m1:mon) (m2:mon) :mon  =
-  if mult_shape_compat (m1.size) (m2.size) then
+  if snd(m1.size) = fst(m2.size) then
   { coeff = m1.coeff*/m2.coeff;
     vars = m1.vars@m2.vars;
     length = m1.length + m2.length;
-    size = mult_shape (m1.size) (m2.size);
+    size = (fst(m1.size),snd(m2.size));
   }
- else if shape_eq (m2.size) (neg_shape) then
+ else if m2.size=(-1,-1) then
    null_mon 
- else if shape_eq (m1.size) (neg_shape) then
+ else if m1.size=(-1,-1) then
    null_mon 
  else
-   Util.tacerror "Monoms sizes uncompatible: %s and %s" (pp_shape m1.size) (pp_shape
-   m2.size)
+   failwith "Monoms sizes uncompatible";;
 
 exception NotPrefix;;
 
@@ -185,7 +150,7 @@ let morder_lt m1 m2 =
 (* ------------------------------------------------------------------------- *)
 
 let mpoly_cmul c (pol:pol) :pol =
-  if c = Int 0 then failwith "got here"
+  if c = Int 0 then []
   else map (fun m -> {m with coeff=c*/m.coeff}) pol;;
 
 let mpoly_mmul cm (pol:pol) :pol = map (mmul cm) pol;;
@@ -239,6 +204,12 @@ let mpoly_muls ps =
      List.fold_left (fun p acc -> mpoly_mul p acc ) p ps;;
 
 
+mpoly_muls  [[{coeff = Int 1; vars = [1]; length = 1; size = (2, 2)};
+    {coeff = Int (-1); vars = [-1]; length = 0; size = (-1, -1)};
+    {coeff = Int 1; vars = [-2]; length = 0; size = (-1, -1)}];
+   [{coeff = Int 1; vars = [1]; length = 1; size = (2, 2)};
+    {coeff = Int (-1); vars = [-1]; length = 0; size = (-1, -1)};
+    {coeff = Int 1; vars = [-2]; length = 0; size = (-1, -1)}]];;
 
 let s_poly (p1:pol) (p2:pol) =
   match (p1,p2) with
@@ -494,7 +465,7 @@ let deduce (p:pol) (polys:pol list)=
    | None ->
       (
         match acc with
-        |[] -> raise Not_found   (* if we already considered all the possible critical pairs, it is over *)
+        |[] -> failwith "No inverter found"   (* if we already considered all the possible critical pairs, it is over *)
         |[]::accs -> aux_get_inv p base accs
         |pol::accs ->
           let s_polys = new_Spolys pol base in
@@ -522,25 +493,109 @@ let sort_poly : pol -> pol = fun p ->
         | (false, true) -> -1
         | (true, false) -> 1
         | (false, false) -> 0
-        | (true, true) -> failwith "unreachable"
+        | (true, true) -> failwith "unreach"
         ) p
 
 
 let sort_polys : pol list -> pol list =
     map sort_poly
 
-let inverter (p:pol) (oldpolys:pol list)=
+let inverter (p:pol) (polys:pol list)=
   let acc = ref 0 in
   let polys = List.map (fun pol ->
                   acc := !acc - 1;
-                pol@[{coeff=Num.Int 1; vars=[!acc]; size=neg_shape;length=0}])
-              (sort_polys oldpolys)
+                pol@[{coeff=Num.Int 1; vars=[!acc]; size=(-1,-1);length=0}])
+  (sort_polys polys)
   in
-  try
   let inv = aux_get_inv (sort_poly p) (DBase.from_list polys) polys in
-  mpoly_cmul (Int (-1)) inv
-  with
-  Not_found ->
-      raise Not_found
+  mpoly_cmul (Int (-1)) inv;;
 
-end
+(* Exemples *)
+(*
+let m1 = {coeff=Num.Int 1; vars=[27]; size=(2,2); length=1};;
+let m2 = {coeff=Num.Int 1; vars=[27;78]; size=(2,4); length=2};;
+let m3 = {coeff=Num.Int 1; vars=[27;27;78]; size=(2,4); length=3};;
+let m4 = {coeff=Num.Int 1; vars=[27;27]; size=(2,2); length=2};;
+let m5 = {coeff=Num.Int 1; vars=[78]; size=(2,4); length=1};;
+
+let p1 = mpoly_add [m1] [m2];;
+mpoly_mul [m1] [m2;m1];;
+
+let base = DBase.from_list [[m1];[m2];[m2;m1];[m4];[m5]];;
+DBase.get_all_prefix_lt base [1;2] ;;
+
+get_all_products [1;2]   (DBase.from_list [[m1];[m2];[m2;m1];[m4];[m5]]);;
+
+let base2 =  DBase.from_list [[m3];[m5]];;
+
+monom_critical_pairs [1;1] base;;
+monom_critical_pairs [1;1] base2;;
+
+reduce_1 [m3] (DBase.from_list [[m2;m4];[m5;m3]]);;
+reduce [m3;m1] (DBase.from_list [[m4;m1];[m5];[m2;m1];[m1]]);;
+
+
+let lb =  [[m2];[m2;m1]];;
+get_all_products m1.vars (DBase.from_list lb);;
+reduce_1 [m1] (DBase.from_list lb);;
+deduce [m1] lb;;
+deduce [m2] lb;;
+deduce [m3] lb;;
+deduce [m4] lb;;
+deduce [m5] lb;;
+inverter [m1] lb;;
+inverter [m2] lb;;
+inverter [m3] lb;;
+inverter [m4] lb;;
+(*inverter [m5] lb;;*)
+
+*)
+
+
+let (x:mon) = {coeff=Num.Int 1; vars = [1]; size=(2,3); length=1};;
+let (yz : mon) = {coeff=Num.Int 1; vars = [2;3]; size=(2,3); length=2};;
+let (a : mon) = {coeff=Num.Int 1; vars = [4]; size=(3,4); length=1};;
+ 
+let xa = {coeff=Num.Int 1; vars=[1;4]; size=(2,4); length=2};;
+let yza = {coeff = Num.Int 1; vars=[2;3;4]; size=(2,4); length = 3};;
+ 
+inverter ([xa; yza]) [[x; yz]; [a]];;
+
+
+(*
+let oldp = 
+    [{coeff=Num.Int 1; vars = [77;47;48]; size=(1,1); length=3}];;
+
+let mybase = 
+    [[{coeff=Num.Int 1; vars = [1]; size=(2,3); length=1}];
+
+
+    let m = 23 in
+    let n = 17 in
+[[{coeff=Num.Int 1; vars = [30]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [33]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [36]; size=(n,m); length=1}];
+[{coeff=Num.Int 1; vars = [40]; size=(m+1,1); length=1}];
+[{coeff=Num.Int 1; vars = [66]; size=(1,m); length=1}];
+[{coeff=Num.Int 1; vars = [68]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [47]; size=(m,n); length=1}];
+[{coeff=Num.Int 1; vars = [48]; size=(n,1); length=1}];
+[{coeff=Num.Int 1; vars = [36;35]; size=(n,1); length=2}];
+[{coeff=Num.Int 1; vars = [68]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [69]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [77;47]; size=(1,n); length=2}];
+[{coeff=Num.Int 1; vars = [92]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [93]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [47]; size=(m,n); length=1}];
+[{coeff=Num.Int 1; vars = [41]; size=(1,m+1); length=1}];
+[{coeff=Num.Int 1; vars = [67]; size=(m,1); length=1}];
+[{coeff=Num.Int 1; vars = [69]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [36]; size=(n,m); length=1}];
+[{coeff=Num.Int 1; vars = [94]; size=(1,n); length=1}];
+[{coeff=Num.Int 1; vars = [77;47]; size=(1,n); length=2}];
+[{coeff=Num.Int 1; vars = [69]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [68]; size=(1,1); length=1}];
+[{coeff=Num.Int 1; vars = [36;35]; size=(n,1); length=2}]];;
+
+inverter oldp mybase;;
+*)
